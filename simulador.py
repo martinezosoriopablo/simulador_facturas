4ad6d6a254886a445054ec81cf79f03b75fe2e20
martinezosoriopablo <54077076+martinezosoriopablo@users.noreply.github.com>
2025-07-15 14:23:23 -0400

    Add files via upload

--- simulador.py
+++ simulador.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Jul 15 09:21:34 2025
 
-@author: marti
+@author: Pablo Martínez
 """
 import streamlit as st
 import datetime
@@ -18,12 +18,12 @@ monto_factura = st.sidebar.number_input("Monto de la factura", min_value=1000.0,
 
 producto = st.sidebar.selectbox("Producto", [
     "Arándanos", "Uvas", "Bananas", "Enlatados", "Aceite", 
-    "Aguacate", "Congelados", "Harina", "Polipropileno", "Cerezas"
+    "Aguacate", "Congelados", "Harina", "Polipropileno", "Cerezas", "Otros"
 ])
 haircuts = {
     "Arándanos": 0.25, "Uvas": 0.25, "Bananas": 0.25, "Enlatados": 0.10, "Aceite": 0.15,
-    "Aguacate": 0.20, "Congelados": 0.15, "Harina": 0.10, "Polipropileno": 0.10, "Cerezas": 0.25
-}
+    "Aguacate": 0.20, "Congelados": 0.15, "Harina": 0.10, "Polipropileno": 0.10, "Cerezas": 0.40, "Otros"
+: 0.30}
 haircut = haircuts[producto]
 
 riesgo = st.sidebar.selectbox("Clasificación de riesgo", ["A", "B", "C"])
@@ -51,13 +51,14 @@ tasa_atraso_diaria = (1 + tasa_atraso_mensual)**(1/30) - 1
 interes_atraso = monto_factura * ((1 + tasa_atraso_diaria)**dias_atraso - 1) if dias_atraso > 0 else 0
 
 monto_financiado = monto_factura * (1 - haircut)
+monto_retenido = monto_factura * (haircut)
 interes = monto_financiado * tasa_periodo
 seguro_credito = monto_factura * seguro_credito_rate
 seguro_carga = monto_factura * seguro_carga_rate
 costo_admin_total = monto_financiado * costo_administracion
 gastos_afectos_iva = costo_admin_total + seguro_credito + seguro_carga
 iva_total = gastos_afectos_iva * iva
-gastos_totales = interes + interes_atraso + gastos_afectos_iva + iva_total
+gastos_totales = interes + gastos_afectos_iva + iva_total
 monto_a_girar = monto_financiado - gastos_totales
 tir_efectiva = ((monto_financiado / monto_a_girar)**(360 / plazo_dias)) - 1 if monto_a_girar > 0 else 0
 
@@ -84,6 +85,7 @@ html = f"""
       <li>Giro del préstamo: <strong>{fecha_giro.strftime('%d-%b-%Y')}</strong></li>
       <li>Pago esperado: <strong>{fecha_pago.strftime('%d-%b-%Y')}</strong></li>
       <li>Días de financiamiento: <strong>{plazo_dias} días</strong></li>
+      <li>Días de atraso: <strong>{dias_atraso} días</strong></li>
     </ul>
   </div>
 
@@ -95,8 +97,7 @@ html = f"""
   <div style="border: 2px solid {COLOR_PRINCIPAL}; padding: 20px 25px; border-radius: 10px; margin-bottom: 20px;">
     <h3>Descuentos 📄</h3>
     <ul style='font-size: 16px; padding-left: 20px;'>
-      <li>Intereses ({tasa_mensual*100:.2f}% mensual): <strong>USD ${interes:,.2f}</strong></li>
-      <li>Intereses por atraso (2.50% mensual): <strong>USD ${interes_atraso:,.2f}</strong></li>
+      <li>Intereses ({tasa_mensual*100:.2f}% mensual): <strong>USD ${interes:,.2f}</strong></li>      
       <li>Costo Administrativo: <strong>USD ${costo_admin_total:,.2f}</strong></li>
       <li>Seguro de Crédito: <strong>USD ${seguro_credito:,.2f}</strong></li>
       <li>Seguro de Carga: <strong>USD ${seguro_carga:,.2f}</strong></li>
@@ -108,13 +109,19 @@ html = f"""
 
   <div style='
     background-color: {COLOR_PRINCIPAL};
-    padding: 30px;
+    padding: 24px;
     border-radius: 15px;
     color: white;
     text-align: left;
-    box-shadow: 0 4px 12px rgba(0,0,0,0.15);'>
-    <h2>Monto a Girar: USD ${monto_a_girar:,.2f}</h2>
-    <h3>TIR Efectiva: {tir_efectiva*100:.2f}% anual</h3>
+    box-shadow: 0 4px 12px rgba(0,0,0,0.1);'>
+    <h3>Monto a Girar: </h3>
+    <div style='font-size: 24px; font-weight: bold;'>USD ${monto_a_girar:,.2f}</div>
+    <h3>Intereses Mora: </h3>
+    <div style='font-size: 24px; font-weight: bold;'>USD ${interes_atraso:,.2f}</div>
+    <h3>Monto Retenido - Intereses Mora: </h3>
+    <div style='font-size: 24px; font-weight: bold;'>USD ${monto_retenido-interes_atraso:,.2f}</div>       
+    <h3>TIR Efectiva:</h3>
+    <div style='font-size: 24px; font-weight: bold;'> {tir_efectiva*100:.2f}% anual</div> 
     <p style='font-size: 14px;'>* Considerando intereses y costos cobrados por adelantado</p>
   </div>
 
